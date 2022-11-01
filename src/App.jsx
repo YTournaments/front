8584a24cce78e514cb01f32e8f1035bae17875be
Anthony <anthony.dole.pro@gmail.com>
2022-11-01 17:10:13 +0100

    Add auth login register

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AppLayout from "./common/layout/AppLayout";
 import AuthLayout from "./common/layout/AuthLayout";
 import Landing from "./common/pages/Landing";
+import Signup from "./common/pages/Signup";
+import Login from "./common/pages/Login";
 function App() {
   const theme = createTheme({
     palette: { mode: "light" },
@@ -13,10 +15,12 @@ function App() {
       <CssBaseLine>
         <BrowserRouter>
           <Routes>
+            <Route path="/" element={<Landing />} />
             <Route path="/" element={<AuthLayout />}>
-              <Route path="landing" element={<Landing />} />
+              <Route path="login" element={<Login />} />
+              <Route path="signup" element={<Signup />} />
             </Route>
-            <Route path="/" element={<AppLayout />}></Route>
+            <Route path="/app" element={<AppLayout />}></Route>
           </Routes>
         </BrowserRouter>
       </CssBaseLine>
