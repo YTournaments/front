112d1740ff2f815770bbd5a53f8a848631777c04
Anthony <anthony.dole.pro@gmail.com>
2022-11-06 19:55:07 +0100

    add custom button

--- src/common/components/Button.jsx
+++ src/common/components/Button.jsx
@@ -1,6 +1,18 @@
-import { styled } from "@mui/material/styles";
-import MuiButton from "@mui/material/Button";
+import { styled } from "@mui/material";
+import { Button } from "@mui/material";
+import { theme } from "../../theme";
 
-export const Button = styled(MuiButton)(({ color }) => ({
-  color: color === "primary" ? "#ae46d" : "#000",
-}));
+export const CustomButton = styled(Button)((props) => {
+  const { variant, color, size, text, onClick, ...others } = props;
+
+  return {
+    ...others,
+    variant: variant || "contained",
+    color: color || "primary",
+    size: size || "large",
+    text: text || "Button",
+    onClick: onClick || (() => {}),
+  };
+});
+
+export default CustomButton;
