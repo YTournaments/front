8753865370c7b7f00b93c7422c6921504429c607
Anthony <anthony.dole.pro@gmail.com>
2022-11-10 15:16:51 +0100

    add error page

--- src/common/hooks/useSignup.jsx
+++ src/common/hooks/useSignup.jsx
@@ -10,11 +10,14 @@ export const useSignup = () => {
     setIsLoading(true);
     setError(null);
 
-    const response = await fetch("http://localhost:5000/api/users/register", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ name, email, password }),
-    });
+    const response = await fetch(
+      "https://ytournaments-apui.onrender.com/api/users/register",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ name, email, password }),
+      }
+    );
     const json = await response.json();
 
     if (!response.ok) {
