606630cfd129b9a75e15011ad24ab6f6c0217f83
Anthony <anthony.dole.pro@gmail.com>
2022-11-10 10:14:56 +0100

    add error page

--- src/common/hooks/useAxiosFetch.jsx
+++ src/common/hooks/useAxiosFetch.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8080/api";
+axios.defaults.baseURL = "http://localhost:5000/api";
 
 export const useAxiosFetch = (axiosParams) => {
   const [data, setData] = useState(undefined);
