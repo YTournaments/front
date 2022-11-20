51aa83df87f739b44476c3444dba0e2aeec3ebf5
Anthony <anthony.dole.pro@gmail.com>
2022-11-20 18:16:58 +0100

    add create tournament

--- src/App.jsx
+++ src/App.jsx
@@ -10,30 +10,39 @@ import Login from "./common/pages/Login";
 import Home from "./common/pages/Home";
 import Error from "./common/pages/Error";
 import Post from "./common/pages/Post";
+import CreateTournament from "./common/pages/tournaments/createTournament";
 import { theme } from "./theme";
 import DevBlog from "./common/pages/devBlog";
+import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseLine>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Landing />} />
+        <LocalizationProvider dateAdapter={AdapterMoment}>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Landing />} />
 
-            <Route path="/" element={<AuthLayout />}>
-              <Route path="login" element={<Login />} />
-              <Route path="signup" element={<Signup />} />
-            </Route>
-            <Route path="/" element={<AppLayout />}>
-              <Route path="home" element={<Home />} />
-              <Route path="blog" element={<DevBlog />} />
-            </Route>
-            <Route path="/admin" element={<AdminLayout />}>
-              <Route path="blog" element={<Post />} />
-            </Route>
-            <Route path="*" element={<Error />} />
-          </Routes>
-        </BrowserRouter>
+              <Route path="/" element={<AuthLayout />}>
+                <Route path="login" element={<Login />} />
+                <Route path="signup" element={<Signup />} />
+              </Route>
+              <Route path="/" element={<AppLayout />}>
+                <Route path="home" element={<Home />} />
+                <Route path="blog" element={<DevBlog />} />
+                <Route
+                  path="tournament/create"
+                  element={<CreateTournament />}
+                />
+              </Route>
+              <Route path="/admin" element={<AdminLayout />}>
+                <Route path="blog" element={<Post />} />
+              </Route>
+              <Route path="*" element={<Error />} />
+            </Routes>
+          </BrowserRouter>
+        </LocalizationProvider>
       </CssBaseLine>
     </ThemeProvider>
   );
