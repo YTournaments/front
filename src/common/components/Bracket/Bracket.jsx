493970c3d7d61af9e65d2a66e70cd70cd1836a0f
Anthony <anthony.dole.pro@gmail.com>
2022-12-30 18:11:04 +0100

    add bracket persisitent

--- src/common/components/Bracket/Bracket.jsx
+++ src/common/components/Bracket/Bracket.jsx
@@ -1,5 +1,3 @@
-import generator from "tournament-generator";
-import _ from "lodash";
 import {
   Bracket,
   Seed,
@@ -8,137 +6,14 @@ import {
   SingleLineSeed,
 } from "react-brackets";
 import { Box, Container } from "@mui/material";
-import { indigo, purple, yellow } from "../../../colors";
+import { purple, yellow } from "../../../colors";
 import { useNavigate } from "react-router-dom";
-export const BracketGame = ({ participants }) => {
-  const checkValues = (number) => {
-    if (number === 1) return 1;
-    const newValue = number / 2;
 
-    if (newValue === 1) {
-      return 1;
-    }
+export const BracketGame = ({ bracket }) => {
+  const rounds = bracket.seeds ? bracket.seeds : [];
 
-    return checkValues(newValue) + 1;
-  };
-
-  const player = [
-    { name: "team A", category: "test" },
-    { name: "team B", category: "test" },
-    { name: "team C", category: "test" },
-    { name: "team D", category: "test" },
-    { name: "team E", category: "test" },
-    { name: "team F", category: "test" },
-    { name: "team G", category: "test" },
-    { name: "team H", category: "test" },
-    { name: "team I", category: "test" },
-    { name: "team J", category: "test" },
-    { name: "team I", category: "test" },
-    { name: "team J", category: "test" },
-    { name: "team I", category: "test" },
-    { name: "team J", category: "test" },
-    { name: "team I", category: "test" },
-    { name: "team J", category: "test" },
-    { name: "team J", category: "test" },
-  ];
-  const players = participants.map((participant) => {
-    return {
-      name: participant.name,
-      category: "test",
-    };
-  });
-
-  const { data: games } = generator(players, {
-    type: "simple-cup",
-    toBeDefinedValue: "-----",
-  });
-
-  const tournamentGames = _.groupBy(games, "round");
-
-  const days = Object.keys(tournamentGames).map((day) => {
-    const dayMatchs = tournamentGames[day];
-    return {
-      title: `Round  ${day}`,
-      seeds: dayMatchs.map((match) => {
-        const home =
-          match.homeTeam === "-----"
-            ? { name: match.homeTeam }
-            : match.homeTeam;
-        const away =
-          match.awayTeam === "-----"
-            ? { name: match.awayTeam }
-            : match.awayTeam;
-        const teams = [
-          {
-            ...home,
-            id: match.customData?.homeTeam ? match.customData.homeTeam : "",
-          },
-          {
-            ...away,
-            id: match.customData?.awayTeam ? match.customData.awayTeam : "",
-          },
-        ];
-        return {
-          id: match.id,
-          teams,
-        };
-      }),
-    };
-  });
-
-  if (days.length > 1 && days[0].seeds.length < days[1].seeds.length * 2) {
-    const emptyMatches = days[1].seeds.length * 2 - days[0].seeds.length;
-    for (let i = 0; i < emptyMatches; i++) {
-      days[0].seeds.push({
-        id: `empty-${i}`,
-        teams: [
-          {
-            name: "-----",
-          },
-          {
-            name: "-----",
-          },
-        ],
-      });
-    }
-  }
-
-  const missings = checkValues(days[days.length - 1].seeds.length);
-
-  const finals = Array.from({ length: missings }, (x, i) => Math.pow(2, i))
-    .reverse()
-    .map((seeds, index) => {
-      const lastDay = days.length + index;
-      return {
-        title: `Round ${lastDay + 1}`,
-        seeds: Array.from({ length: seeds }).map((m, idx) => {
-          const prevMatch = lastDay > days.length ? false : days[lastDay - 1];
-
-          return {
-            id: `MS${index}${idx}`,
-            teams: [
-              {
-                name: "----",
-                id: prevMatch.seeds
-                  ? prevMatch.seeds[2 * index].id
-                  : `MS${index - 1}${2 * idx}`,
-              },
-              {
-                name: "-----",
-                id: prevMatch.seeds
-                  ? prevMatch.seeds[2 * index + 1].id
-                  : `MS${index - 1}${2 * idx + 1}`,
-              },
-            ],
-          };
-        }),
-      };
-    });
-
-  const rounds = [...days, ...finals];
   const CustomSeed = ({ seed, title, breakpoint, roundIndex, seedIndex }) => {
     const navigate = useNavigate();
-    console.log(seed);
     const [home, away] = seed.teams;
     if (seed.teams !== undefined || seed.teams !== null || seed.teams !== "") {
       return (
@@ -203,14 +78,18 @@ export const BracketGame = ({ participants }) => {
           alignItems: "center",
         }}
       >
-        <Bracket
-          rounds={rounds}
-          renderSeedComponent={CustomSeed}
-          swipeableProps={{
-            enableMouseEvents: true,
-            animateHeight: true,
-          }}
-        />
+        {bracket.seeds ? (
+          <Bracket
+            rounds={rounds}
+            renderSeedComponent={CustomSeed}
+            swipeableProps={{
+              enableMouseEvents: true,
+              animateHeight: true,
+            }}
+          />
+        ) : (
+          <div>Chargement...</div>
+        )}
       </Box>
     </Container>
   );
